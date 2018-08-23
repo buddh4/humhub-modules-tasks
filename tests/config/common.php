4b4280a1504808147d7d32ddd775a8e8c85af4d3
buddh4 <mail@jharrer.de>
2018-08-24 00:36:48 +0200

    travis autoload path

--- tests/config/common.php
+++ tests/config/common.php
@@ -2,4 +2,6 @@
 /**
  * This config is shared by all suites (unit/functional/acceptance) and can be overwritten by a suite config (e.g. functional.php)
  */
-return [];
+return [
+    'moduleAutoloadPaths' => ['/home/travis/build/buddh4']
+];
